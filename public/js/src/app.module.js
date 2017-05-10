10b07e353380e7f025cfafd5e446c619d1eb63eb
Andrew Schneider <andrew.mark.schneider@gmail.com>
2017-05-09 19:55:01 -0500

    new tweet html/styles and removal of old code

--- public/js/src/app.module.js
+++ public/js/src/app.module.js
@@ -3,9 +3,7 @@
 
   angular
     .module('mnTweets', ['ngMap', 'ngRoute', 'ngMaterial'])
-    .config(["$routeProvider", "$mdThemingProvider", "$mdIconProvider",
-    function ($routeProvider, $mdThemingProvider, $mdIconProvider) {
-
+    .config(["$routeProvider", "$mdThemingProvider", function ($routeProvider, $mdThemingProvider) {
       // route configurations
       $routeProvider
         .when("/", {
@@ -17,14 +15,6 @@
           controller: "streamCtrl"
         })
         .otherwise({redirectTo: "/"});
-
-      // icon configurations
-      $mdIconProvider
-        .icon("menu", "images/menu.svg", 24)
-        .icon("twitter-retweet", "images/twitter-buttons/retweet.svg", 24)
-        .icon("twitter-details", "images/twitter-buttons/details.svg", 24)
-        .icon("twitter-favorite", "images/twitter-buttons/favorite.svg", 24)
-        .icon("twitter-reply", "images/twitter-buttons/reply.svg", 24);
 
       // theme configurations
       $mdThemingProvider.theme('default')
